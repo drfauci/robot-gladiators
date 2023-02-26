04a084a504c395c2d186154a7808ed545d337627
justin <justinalfieri20@gmail.com>
2023-02-26 17:26:52 -0500

    functions,expressions, and variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("this is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("what is your robot's name");
+console.log(playerName)
+console.log("This logs a string, good for leaving yourslef a message");
+// this will do math and log 20
+console.log(10 + 10);
+//whats this? 
+console.log("Our robots name is " + playerName);
+var name = "justin alfieri";
+console.log(name);
+function fight(){
+window.alert("FUCK YOU ");
+}
+// fight ();
